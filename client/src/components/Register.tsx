8238dc67045519804633980e88caee4f382c5bfc
AlexsanderSouza <alexsander.rafael2022@gmail.com>
2025-07-10 12:59:27 -0400

    feat: implement login and registration functionality with API integration

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,55 +1,85 @@
-import React from 'react'
+import { useState } from "react";
+import { api } from "../utils/api";
+import { useNavigate } from "react-router-dom";
+
 
 const Register = () => {
+
+const navigate = useNavigate();
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [name, setName] = useState("");
+const [loading, setLoading] = useState(false);
+
+const handleRegister = async (e: React.FormEvent) => {
+  e.preventDefault();
+  setLoading(true);
+
+  try {
+    await api.post("auth/register", { name, email, password });
+    navigate("/dashboard");
+  } catch (err) {
+    console.error("Registration error:", err);
+  }
+  finally {
+    setLoading(false);
+  }
+}
+
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-
-    <div>
-        <label className="block text-sm/6 font-medium text-gray-900">Username</label>
-        <div className="mt-2">
-          <input type="text" name="username" 
-          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
       </div>
 
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleRegister}>
 
-                <div>
-        <label className="block text-sm/6 font-medium text-gray-900">Email address</label>
-        <div className="mt-2">
-          <input type="email" name="email" 
-          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
+          <div>
+            <label className="block text-sm/6 font-medium text-gray-900">Username</label>
+            <div className="mt-2">
+              <input type="text" name="username" value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
 
-      <div>
-        <div className="flex items-center justify-between">
-            <label className="block text-sm/6 font-medium text-gray-900">Password</label>
-        </div>
-        <div className="mt-2">
-          <input type="password" name="password"
-          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
-      <div>
-        <button type="submit" 
-        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-        Register</button>
-      </div>
-      
-        <div className="text-sm">
+
+          <div>
+            <label className="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <div className="mt-2">
+              <input type="email" name="email" value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label className="block text-sm/6 font-medium text-gray-900">Password</label>
+            </div>
+            <div className="mt-2">
+              <input type="password" name="password" value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
+          <div>
+            <button type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Register</button>
+          </div>
+
+          <div className="text-sm">
             <a href="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                Click here to login
+              Click here to login
             </a>
           </div>
-      
-            </form>
-        </div>
+
+        </form>
+      </div>
     </div>
   )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import sequelize from './config/connection';
 import routes from './routes/index';
+import dotenv from 'dotenv';
+import { corsMiddleware } from './middleware/corsMiddleware';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware to handle CORS
+app.use(corsMiddleware);
 
 // Middleware to parse JSON requests
 app.use(express.json());
